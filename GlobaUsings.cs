50ebeb62abee51dd432f7547c3954e891ff87bbb
Moonenm <u0157634@thomasmore.be>
2024-02-19 15:30:38 +0100

    Add project files.

---
+++ GlobaUsings.cs
@@ -0,0 +1,4 @@
+﻿global using CommunityToolkit.Mvvm.ComponentModel;
+global using CommunityToolkit.Mvvm.Input;
+global using OndernemingMVVM2.ViewModels;
+global using OndernemingMVVM2.Views;
---
+++ Views/MainPage.xaml.cs
@@ -0,0 +1,9 @@
+namespace OndernemingMVVM2.Views;
+
+public partial class MainPage : ContentPage
+{
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+}
